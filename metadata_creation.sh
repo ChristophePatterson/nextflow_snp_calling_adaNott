fbf3e7e02ad03adbe1ba5d52c7765580d02272b4
Christophe Patterson <christophepatterson@outlook.com>
2026-03-31 09:52:28 +0100

    Completed nextflow run 2026-03-31 and renaming for RepAdapt convension

--- metadata_creation.sh
+++ metadata_creation.sh
@@ -5,36 +5,42 @@ module purge
 module load R-uoneasy/4.2.1-foss-2022a
 module load bcftools-uoneasy/1.18-GCC-13.2.0
 
+# Repadapt run name
+run_version="RepAdapt_NF_GasAcu_fw_2026-03-26"
+
 # Move to working directory
 cd /gpfs01/home/mbzcp2/code/Github/nextflow_snp_calling_adaNott/
 
 ## Get names of smaples that completed the pipeline
 # Print out sample names
-bcftools query -l output/final_variants.vcf.gz > complete_NF_samps.txt
+bcftools query -l output/$run_version/final_variants.vcf.gz > complete_NF_samps.txt
 
 # Run script for creating metadata
 Rscript metadata_creation.R complete_NF_samps.txt seq/fw_seq_data.csv
 
 # Rename output files to set as RepAdaptID
 runname="rawg0173_Gasterosteus_aculeatus_Patterson"
-outputdir="output/$runname"
+outputdir="output/$run_version/$runname"
 mkdir $outputdir
 
 # 1) vcf
-cp output/final_variants.vcf.gz $outputdir/$runname.vcf.gz
+cp output/$run_version/final_variants.vcf.gz $outputdir/$runname.vcf.gz
 # 2) Genome seq
 gzip -c /gpfs01/home/mbzcp2/data/sticklebacks/genomes/GCF_016920845.1/GCF_016920845.1_GAculeatus_UGA_version5_genomic.fasta > $outputdir/$runname.fasta.gz
 # 3) Genome annotation file
 gzip -c /gpfs01/home/mbzcp2/data/sticklebacks/genomes/GCF_016920845.1/genomic.gff > $outputdir/$runname.gff.gz
 # 4) Genome amino acid seq
 cp /gpfs01/home/mbzcp2/data/sticklebacks/genomes/GCF_016920845.1/GCF_016920845.1_GAculeatus_UGA_version5_protein.faa.gz $outputdir/$runname.faa.gz
-# 5) Gene depth (orignal)
-
+# 5) Gene depth (No longer required)
+# ------
 # 6) Gene depth denovo
-cp output/combined_genes.tsv.gz $outputdir/$runname.depth_gene_denovo.gz
+cp output/$run_version/combined_genes.tsv $outputdir/$runname.depth_gene_denovo.gz
 
 # 7) Window depth
-cp output/combined_windows.tsv.gz $outputdir/$runname.depth_gene_5Kb.gz
+cp output/$run_version/combined_windows.tsv $outputdir/$runname.depth_gene_5Kb.gz
 
 # 8) Whole genome depth
-gzip -c output/combined_wg.tsv > $outputdir/$runname.depth_wg.gz
+gzip -c output/$run_version/combined_wg.tsv > $outputdir/$runname.depth_wg.gz
+
+# Meta data
+cp RepAdapt_rawg0173_Gasterosteus_aculeatus_Patterson_metadata.txt $outputdir/RepAdapt_rawg0173_Gasterosteus_aculeatus_Patterson_metadata.txt 
